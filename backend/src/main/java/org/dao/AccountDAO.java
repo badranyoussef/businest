cd97859b443e8a5410d647cf3367c6f07d05d505
lassekh <31202809+lassekh@users.noreply.github.com>
2024-11-12 15:15:28 +0100

    test

--- backend/src/main/java/org/dao/AccountDAO.java
+++ backend/src/main/java/org/dao/AccountDAO.java
@@ -10,24 +10,24 @@ import java.util.Scanner;
 
 public class AccountDAO {
 
-    public static List<Account> createAccountList() {
+    public List<Account> createAccountList() {
         List<Account> accountList = new ArrayList<>();
-        accountList.add(new Account(1, "Alex Johnson", Role.COMPANY_MANAGER));
-        accountList.add(new Account(2, "Jamie Smith", Role.EMPLOYEE));
-        accountList.add(new Account(3, "Taylor Brown", Role.EMPLOYEE));
-        accountList.add(new Account(4, "Morgan White", Role.COMPANY_MANAGER));
-        accountList.add(new Account(5, "Casey Black", Role.HR));
-        accountList.add(new Account(6, "Jordan Green", Role.EMPLOYEE));
-        accountList.add(new Account(7, "Riley Grey", Role.EMPLOYEE));
-        accountList.add(new Account(8, "Avery Gold", Role.COMPANY_MANAGER));
-        accountList.add(new Account(9, "Cameron Blue", Role.EMPLOYEE));
-        accountList.add(new Account(10, "Dakota Silver", Role.EMPLOYEE));
+        accountList.add(new Account(1, "Alex Johnson", new Role("Company Manager")));
+        accountList.add(new Account(2, "Jamie Smith", new Role("Employee")));
+        accountList.add(new Account(3, "Taylor Brown", new Role("Employee")));
+        accountList.add(new Account(4, "Morgan White", new Role("Company Manager")));
+        accountList.add(new Account(5, "Casey Black", new Role("HR")));
+        accountList.add(new Account(6, "Jordan Green", new Role("Employee")));
+        accountList.add(new Account(7, "Riley Grey", new Role("Employee")));
+        accountList.add(new Account(8, "Avery Gold", new Role("Company Manager")));
+        accountList.add(new Account(9, "Cameron Blue", new Role("Employee")));
+        accountList.add(new Account(10, "Dakota Silver", new Role("Employee")));
 
         return accountList;
     }
 
     private static AccountDAO instance;
-
+    @Getter
     private List<Account> accounts = createAccountList();
 
     public static AccountDAO getInstance() {
@@ -46,6 +46,14 @@ public class AccountDAO {
     public AccountDTO updateAccount(AccountDTO dto) {
         for (Account account : accounts) {
             if (account.getId() == dto.getId()) {
+                account.updateRole(new Role(dto.getRole()));
+
+
+                /*Role newRole = new Role(dto.getRole());
+                Account updatedAccount = account;
+                updatedAccount.updateRole(newRole);
+                accounts.remove(account);
+                accounts.add(updatedAccount);*/
                 return new AccountDTO(account);
             }
         }
