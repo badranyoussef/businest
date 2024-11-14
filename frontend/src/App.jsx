e8719589bb0f493c5ddd4cca409b39fd5dbfb3ac
SebastianMandrup <46905237+SebastianMandrup@users.noreply.github.com>
2024-11-14 10:52:40 +0100

    introduced not found page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Navbar } from './components/Navigation/Navbar';
-import { RoleList } from './components/RoleList/RoleList';
-import { Profile } from './components/Profile/Profile';
-import './styles/App.css';
-import { FolderList } from './components/FolderList/FolderList';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navigation/Navbar";
+import { RoleList } from "./components/RoleList/RoleList";
+import { Profile } from "./components/Profile/Profile";
+import "./styles/App.css";
+import { FolderList } from "./components/FolderList/FolderList";
+import { NotFound } from "./components/NotFound/notFound";
 
 export function App() {
   return (
@@ -14,8 +14,9 @@ export function App() {
         <main className="main-content">
           <Routes>
             <Route path="/" element={<FolderList />} />
-            <Route path='/roles' element={<RoleList />} />
+            <Route path="/roles" element={<RoleList />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </main>
       </div>
