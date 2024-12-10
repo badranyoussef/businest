af350a697130e6e23a291f86dd2ffd0215098948
badranyoussef <79192971+badranyoussef@users.noreply.github.com>
2024-12-10 13:56:25 +0100

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { AccountManagementPage } from './pages/AccountManagementPage';
+import { AccountPage } from './pages/AccountPage';
+import FolderConfig from "./components/FolderConfig/FolderConfig";
+import { FolderList } from "./components/FolderAdminList/FolderAdminList";
+import { Profile } from "./components/Profile/Profile";
+import { NotFound } from "./components/NotFound/NotFound";
+import { Navbar } from "./components/shared/Navbar/Navbar";
+import Footer from "./components/shared/Footer/Footer";
+import MyLogs from './page/MyLogs';
+import FileList from "./page/FileList";
+import PathFinder from "./page/FileExplorer.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <BrowserRouter>
+      <Navbar />
+      <div className="app-body">
+        <Routes>
+          <Route path="/" element={<Navigate to="/account" replace />} />
+          <Route path="/account" element={<AccountManagementPage />} />
+          <Route path="/account/:id" element={<AccountPage />} />
+          <Route path="/folders" element={<FolderList />} />
+          <Route
+            path="/folders/:folderName/configure"
+            element={<FolderConfig />}
+          />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/logs" element={<MyLogs />} />
+          <Route path="/folderTest" element={<FileList />} />
 
-export default App
+          <Route path="*" element={<NotFound />} />
+          <Route path="/pathfinder" element={<PathFinder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </BrowserRouter>
+  );
+}
+export default App;
